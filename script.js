d170211e78e40ee0d8a3f24f654780b5d70515ae
Robert Nowaczyk <iro8ert@o2.pl>
2018-02-12 22:38:14 +0100

    change loop

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('myButton').addEventListener("click", add);
 function add() {
   var a = document.createElement('li');
   document.getElementById('list').appendChild(a);
-  for (var i = 1; i < document.getElementsByTagName('li').length ; i++) {
+  for (var i = 0; i < document.getElementsByTagName('li').length ; i++) {
     a.innerHTML = "item" + i;
   }
 }
