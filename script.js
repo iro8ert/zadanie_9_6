12ee9ee2951134ff0d44fa5f4d2ab34840238eb4
Robert Nowaczyk <iro8ert@o2.pl>
2018-02-13 20:34:53 +0100

    change script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 
-document.addEventListener("click", add);
-
-
 function add() {
-	document.getElementById('myButton');
-  	var a = document.createElement('li');
+	var a = document.createElement('li');
   	document.getElementById('list').appendChild(a);
   	for (var i = 0; i < document.getElementsByTagName('li').length ; i++) {
     	a.innerHTML = "item" + i;
   	}
 }
+
+
+function load() {
+	var myButton = document.getElementById('myButton');
+	myButton.addEventListener('click', add);
+}
+
